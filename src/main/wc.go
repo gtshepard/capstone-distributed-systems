f3ee3118649a3ab8fe662d82e5141cbb5ff34004
Garrison Shepard <shepard.garrisont@gmail.com>
2019-09-14 18:11:54 -0400

    part 1 complete

--- src/main/wc.go
+++ src/main/wc.go
@@ -1,18 +1,68 @@
 package main
 
-import "os"
-import "fmt"
-import "mapreduce"
-import "container/list"
+// import "os"
+// import "fmt"
+// import "mapreduce/mapreduce"
+// import "container/list"
+
+import (
+	"container/list"
+	"fmt"
+	"log"
+	"mapreduce"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 // our simplified version of MapReduce does not supply a
 // key to the Map function, as in the paper; only a value,
 // which is a part of the input file contents
 func Map(value string) *list.List {
+
+	regRemovePunct, errPunct := regexp.Compile("[^a-zA-Z0-9]+")
+	regRemoveStamp, errStamp := regexp.Compile("\\d{3}:\\d{3}")
+	regRemoveNumber, errStamp := regexp.Compile("\\d")
+
+	if errPunct != nil {
+		log.Fatal(errPunct)
+	}
+
+	if errStamp != nil {
+		log.Fatal(errStamp)
+	}
+
+	//parse text without verse stamp
+	noVerseNumber := regRemoveStamp.ReplaceAllString(value, " ")
+	text := strings.Split(noVerseNumber, " ")
+	keyValueList := list.New()
+
+	for _, element := range text {
+
+		cleanStr := regRemoveNumber.ReplaceAllString(element, "")
+		token := regRemovePunct.ReplaceAllString(cleanStr, "")
+
+		mrKeyValue := mapreduce.KeyValue{
+			Key:   token,
+			Value: "1",
+		}
+
+		keyValueList.PushFront(mrKeyValue)
+	}
+
+	return keyValueList
 }
 
 // iterate over list and add values
 func Reduce(key string, values *list.List) string {
+	//aggregate all values for a given key (sum all values for a key)
+	count := 0
+	for e := values.Front(); e != nil; e = e.Next() {
+		count += 1
+	}
+	frequency := strconv.Itoa(count)
+	return frequency
 }
 
 // Can be run in 3 ways:
@@ -20,17 +70,17 @@ func Reduce(key string, values *list.List) string {
 // 2) Master (e.g., go run wc.go master x.txt localhost:7777)
 // 3) Worker (e.g., go run wc.go worker localhost:7777 localhost:7778 &)
 func main() {
-  if len(os.Args) != 4 {
-    fmt.Printf("%s: see usage comments in file\n", os.Args[0])
-  } else if os.Args[1] == "master" {
-    if os.Args[3] == "sequential" {
-      mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
-    } else {
-      mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])    
-      // Wait until MR is done
-      <- mr.DoneChannel
-    }
-  } else {
-    mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
-  }
+	if len(os.Args) != 4 {
+		fmt.Printf("%s: see usage comments in file\n", os.Args[0])
+	} else if os.Args[1] == "master" {
+		if os.Args[3] == "sequential" {
+			mapreduce.RunSingle(5, 3, os.Args[2], Map, Reduce)
+		} else {
+			mr := mapreduce.MakeMapReduce(5, 3, os.Args[2], os.Args[3])
+			// Wait until MR is done
+			<-mr.DoneChannel
+		}
+	} else {
+		mapreduce.RunWorker(os.Args[2], os.Args[3], Map, Reduce, 100)
+	}
 }
