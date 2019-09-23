f4d875f9a15fec60c99dd952354f250a776e57af
Garrison Shepard <shepard.garrisont@gmail.com>
2019-09-23 15:36:34 -0400

    all tests when when setting nRPC value to -1 pass

--- src/mapreduce/master.go
+++ src/mapreduce/master.go
@@ -105,12 +105,116 @@ func (mr *MapReduce) MakeAllWorkersIdle(workers []*RegisterArgs) {
 }
 
 func (mr *MapReduce) RunMaster() *list.List {
+	//mapSyncChannel := make(chan string, mr.nMap)
+	//reduceSyncChannel := make(chan string, mr.nReduce)
+	// workers := mr.RecieveWorkers()
+	// mr.DistributedMap(workers, mapSyncChannel)
+	// mr.MakeAllWorkersIdle(workers)
+	// mr.DistributedReduce(workers, reduceSyncChannel)
+	//register workers and set them to idle
+	myLogger("RM-1", "START OF TEST", "RunMaster()", "master.go")
+	var workers []*RegisterArgs
+
+	// for i := 0; i < 2; i++ {
+	//  select {
+	//  case worker := <-mr.registerChannel:
+	//      worker.isIdle = true
+	//      myLogger("RM-2", "register worker: "+worker.Worker, "RunMaster() - register-worker", "master.go")
+	//      workers = append(workers, worker)
+	//      info := &WorkerInfo{}
+	//      info.address = worker.Worker
+	//      mr.Workers[worker.Worker] = info
+	//  default:
+	//      myLogger("RM-2", "FAIL TO REGISTER", "RunMaster() - register-worker", "master.go")
+	//  }
+	// }
+
+	//find an idle worker
+	getIdleWorker := func(s []*RegisterArgs) (int, string, bool) {
+		for i := 0; i < len(s); i++ {
+			if s[i].isIdle {
+				//myLogger("RM-3", "Found Idle Worker", "getIdleWorker", "master.go")
+				return i, s[i].Worker, true
+			} else {
+				//myLogger("RM-3", "No Idle Worker", "RunMaster()", "master.go")
+			}
+		}
+		return 0, "", false
+	}
+
+	completedMapJobs := make([]string, 0)
+	completedReduceJobs := make([]string, 0)
 	mapSyncChannel := make(chan string, mr.nMap)
 	reduceSyncChannel := make(chan string, mr.nReduce)
-	workers := mr.RecieveWorkers()
-	mr.DistributedMap(workers, mapSyncChannel)
-	mr.MakeAllWorkersIdle(workers)
-	mr.DistributedReduce(workers, reduceSyncChannel)
+
+	//var mapIdleWg sync.WaitGroup
+	//var mapNonIdleWg sync.WaitGroup
+	//schedule all M map jobs
+	//jobs can only be scheduled on idle workers
+
+	// select {
+	// case worker := <-mr.registerChannel:
+	//  worker.isIdle = true
+	//  myLogger("RM-2", "register worker: "+worker.Worker, "RunMaster() - register-worker", "master.go")
+	//  workers = append(workers, worker)
+	//  info := &WorkerInfo{}
+	//  info.address = worker.Worker
+	//  mr.Workers[worker.Worker] = info
+	// default:
+	//  myLogger("RM-2", "FAIL TO REGISTER", "RunMaster() - register-worker", "master.go")
+	// }
+
+	for i := 0; i < mr.nMap; i++ {
+		select {
+		case worker := <-mr.registerChannel:
+			workers = append(workers, worker)
+			go mr.AssignJobToIdleWorker("Map", i, worker.Worker, mr.nReduce, mapSyncChannel)
+		default:
+			mapJobDone := <-mr.MapJobChannel
+			completedMapJobs = append(completedMapJobs, mapJobDone)
+			go mr.AssignJobToIdleWorker("Map", i, mapJobDone, mr.nReduce, mapSyncChannel)
+			myLogger("RM-2", "FAIL TO REGISTER", "RunMaster() - register-worker", "master.go")
+
+		}
+	}
+
+	newlyAvailibleWorker := <-mr.MapJobChannel
+	completedMapJobs = append(completedMapJobs, newlyAvailibleWorker)
+
+	for i := 0; i < len(workers); i++ {
+		workers[i].isIdle = true
+	}
+
+	for i := 0; i < mr.nReduce; i++ {
+
+		select {
+		case worker := <-mr.registerChannel:
+			workers = append(workers, worker)
+			go mr.AssignJobToIdleWorker("Reduce", i, worker.Worker, mr.nMap, reduceSyncChannel)
+
+		default:
+			index, workerName, isAvailibleWorker := getIdleWorker(workers)
+			if isAvailibleWorker {
+				workers[index].isIdle = false
+				go mr.AssignJobToIdleWorker("Reduce", i, workerName, mr.nMap, reduceSyncChannel)
+			} else {
+				reduceJobDone := <-mr.ReduceJobChannel
+				go mr.AssignJobToIdleWorker("Reduce", i, reduceJobDone, mr.nMap, reduceSyncChannel)
+				myLogger("RM-2", "FAIL TO REGISTER", "RunMaster() - register-worker", "master.go")
+			}
+		}
+	}
+
+	Rna := <-mr.ReduceJobChannel
+	completedReduceJobs = append(completedReduceJobs, Rna)
+
+	mr.Merge()
+	return mr.KillWorkers()
+
 	mr.Merge()
 	return mr.KillWorkers()
 }
+
+// how to handle worker failures?
+// a worker is considered failed when an RPC call is made
+// if a worker is considered failed if an RPC call fails
