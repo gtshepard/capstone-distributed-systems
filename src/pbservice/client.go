fba04698a5340af2ce46dc57a1bf405a0e457178
Garrison Shepard <shepard.garrisont@gmail.com>
2019-10-15 21:08:16 -0400

    vs tests all pass

--- src/pbservice/client.go
+++ src/pbservice/client.go
@@ -1,30 +1,29 @@
 package pbservice
 
-import "viewservice"
-import "net/rpc"
-import "fmt"
+import (
+	"fmt"
+	"net/rpc"
+	"strconv"
+	"viewservice"
+)
 
 // You'll probably need to uncomment these:
 // import "time"
 // import "crypto/rand"
 // import "math/big"
 
-
-
 type Clerk struct {
-  vs *viewservice.Clerk
-  // Your declarations here
+	vs *viewservice.Clerk
+	// Your declarations here
 }
-
 
 func MakeClerk(vshost string, me string) *Clerk {
-  ck := new(Clerk)
-  ck.vs = viewservice.MakeClerk(me, vshost)
-  // Your ck.* initializations here
+	ck := new(Clerk)
+	ck.vs = viewservice.MakeClerk(me, vshost)
+	// Your ck.* initializations here
 
-  return ck
+	return ck
 }
-
 
 //
 // call() sends an RPC to the rpcname handler on server srv
@@ -43,20 +42,20 @@ func MakeClerk(vshost string, me string) *Clerk {
 // please don't change this function.
 //
 func call(srv string, rpcname string,
-          args interface{}, reply interface{}) bool {
-  c, errx := rpc.Dial("unix", srv)
-  if errx != nil {
-    return false
-  }
-  defer c.Close()
-    
-  err := c.Call(rpcname, args, reply)
-  if err == nil {
-    return true
-  }
+	args interface{}, reply interface{}) bool {
+	c, errx := rpc.Dial("unix", srv)
+	if errx != nil {
+		return false
+	}
+	defer c.Close()
 
-  fmt.Println(err)
-  return false
+	err := c.Call(rpcname, args, reply)
+	if err == nil {
+		return true
+	}
+
+	fmt.Println(err)
+	return false
 }
 
 //
@@ -68,9 +67,19 @@ func call(srv string, rpcname string,
 //
 func (ck *Clerk) Get(key string) string {
 
-  // Your code here.
+	// Your code here.
+	view, _ := ck.vs.Get()
+	args := &GetArgs{}
+	args.Key = key
+	var reply *GetReply
 
-  return "???"
+	ok := call(view.Primary, "PBServer.Get", args, &reply)
+
+	if ok {
+		return reply.Value
+	} else {
+		return ""
+	}
 }
 
 //
@@ -79,14 +88,52 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutExt(key string, value string, dohash bool) string {
 
-  // Your code here.
-  return "???"
+	// Your code here.
+	//remember to test this function explicitly (the hashing part )
+	// Your code here.
+	view, _ := ck.vs.Get()
+	putArgs := &PutArgs{}
+	var putReply *PutReply
+
+	if dohash {
+
+		getArgs := &GetArgs{}
+		var getReply GetReply
+		getArgs.Key = key
+		//get previous value for a key
+		if ok := call(view.Primary, "PBServer.Get", getArgs, &getReply); !ok {
+			return "ERROR"
+		}
+
+		//hash new value
+		prev := getReply.Value
+		hashedValue := strconv.Itoa(int(hash(prev + value)))
+		putArgs.Key = key
+		putArgs.Value = hashedValue
+		//send key and hashed value pair to database
+		if ok := call(view.Primary, "PBServer.Put", putArgs, putReply); !ok {
+			return "Error"
+		} else {
+			return key
+		}
+
+	} else {
+		// put value not hash
+		putArgs.Key = key
+		putArgs.Value = value
+
+		if ok := call(view.Primary, "PBServer.Put", putArgs, &putReply); !ok {
+			return "ERROR"
+		} else {
+			return key
+		}
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
-  ck.PutExt(key, value, false)
+	ck.PutExt(key, value, false)
 }
 func (ck *Clerk) PutHash(key string, value string) string {
-  v := ck.PutExt(key, value, true)
-  return v
+	v := ck.PutExt(key, value, true)
+	return v
 }
--- src/pbservice/server.go
+++ src/pbservice/server.go
@@ -1,15 +1,18 @@
 package pbservice
 
-import "net"
-import "fmt"
-import "net/rpc"
-import "log"
-import "time"
-import "viewservice"
-import "os"
-import "syscall"
-import "math/rand"
-import "sync"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net"
+	"net/rpc"
+	"os"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+	"viewservice"
+)
 
 //import "strconv"
 
@@ -17,118 +20,220 @@ import "sync"
 const Debug = 0
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
-  if Debug > 0 {
-    n, err = fmt.Printf(format, a...)
-  }
-  return
+	if Debug > 0 {
+		n, err = fmt.Printf(format, a...)
+	}
+	return
 }
 
 type PBServer struct {
-  l net.Listener
-  dead bool // for testing
-  unreliable bool // for testing
-  me string
-  vs *viewservice.Clerk
-  done sync.WaitGroup
-  finish chan interface{}
-  // Your declarations here.
+	l          net.Listener
+	dead       bool // for testing
+	unreliable bool // for testing
+	me         string
+	vs         *viewservice.Clerk
+	done       sync.WaitGroup
+	finish     chan interface{}
+	// Your declarations here.
+	writer    chan *ClientMsg
+	reader    chan *ClientMsg
+	db        map[string]string
+	update    chan *Update
+	repilcate chan *DBCopy
+	ack       chan *SrvAckArgs
+	intervals int
 }
 
 func (pb *PBServer) Put(args *PutArgs, reply *PutReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	msg := &ClientMsg{}
+	msg.Key = args.Key
+	msg.Value = args.Value
+	pb.writer <- msg
+	return nil
 }
 
 func (pb *PBServer) Get(args *GetArgs, reply *GetReply) error {
-  // Your code here.
-  return nil
+	// Your code here.
+	//make request to db
+	msg := &ClientMsg{}
+	msg.Key = args.Key
+	pb.reader <- msg
+	//get reply
+	rep := <-pb.reader
+	reply.Value = rep.Value
+
+	return nil
 }
 
+func (pb *PBServer) RecieveUpdate(args *PutArgs, reply *PutReply) error {
+	update := &Update{}
+	update.Key = args.Key
+	update.Value = args.Value
+	pb.update <- update
+	return nil
+}
+
+func (pb *PBServer) RecieveReplica(args *ReplicateArgs, reply *ReplicateReply) error {
+	replica := &DBCopy{}
+	replica.Db = args.Db
+	pb.repilcate <- replica
+	return nil
+}
+
+func (pb *PBServer) Ack(args *SrvAckArgs, reply *SrvAckReply) error {
+	pb.ack <- args
+	return nil
+}
 
 // ping the viewserver periodically.
 func (pb *PBServer) tick() {
-  // Your code here.
-}
+	// Your code here.
+	pb.intervals += 1
+	myLogger("INTERVAL COUNT: ", strconv.Itoa(pb.intervals)+" SRV: "+pb.me, "Tick", "Server.go")
+	if pb.dead {
+		myLogger("DEAD FLAG", "SRV FAILED: "+pb.me, "Tick", "Server.go")
+		time.Sleep(viewservice.PingInterval * 5)
+	}
 
+	view, _ := pb.vs.Get()
+
+	if view.Viewnum < 2 {
+		view, _ = pb.vs.Ping(0)
+	} else {
+		view, _ = pb.vs.Ping(view.Viewnum)
+	}
+
+	if pb.me == view.Primary {
+
+		select {
+		case read := <-pb.reader:
+
+			msg := &ClientMsg{}
+			if val, ok := pb.db[read.Key]; ok {
+				//read given key from db
+				msg.Key = read.Key
+				msg.Value = val
+				pb.reader <- msg
+			} else {
+				//attempted to read key that does not exist
+				msg.Key = read.Key
+				msg.Value = ""
+				pb.reader <- msg
+			}
+
+		case write := <-pb.writer:
+			pb.db[write.Key] = write.Value
+			args := &PutArgs{}
+			var reply *PutReply
+			args.Key = write.Key
+			args.Value = write.Value
+			myLogger("After Recieve", pb.db[write.Key], "Tick", "Server.go")
+			if ok := call(view.Backup, "PBServer.RecieveUpdate", args, &reply); !ok {
+				myLogger("ReciveUpdate", "RPC FAIL", "Tick", "Server.go")
+				return
+			}
+			// wait for update to be written to backup
+			//<-pb.ack
+		}
+
+	} else if pb.me == view.Backup {
+		select {
+		case replica := <-pb.repilcate:
+			pb.db = replica.Db
+			//call(view.Primary, "PBServer.Ack", args, &reply)
+		case update := <-pb.update:
+			pb.db[update.Key] = update.Value
+			//call(view.Primary, "PBServer.Ack", args, &reply)
+		}
+	} else {
+
+	}
+}
 
 // tell the server to shut itself down.
 // please do not change this function.
 func (pb *PBServer) kill() {
-  pb.dead = true
-  pb.l.Close()
+	pb.dead = true
+	pb.l.Close()
 }
 
-
 func StartServer(vshost string, me string) *PBServer {
-  pb := new(PBServer)
-  pb.me = me
-  pb.vs = viewservice.MakeClerk(me, vshost)
-  pb.finish = make(chan interface{})
-  // Your pb.* initializations here.
+	pb := new(PBServer)
+	pb.me = me
+	pb.vs = viewservice.MakeClerk(me, vshost)
+	pb.finish = make(chan interface{})
+	// Your pb.* initializations here.
+	pb.writer = make(chan *ClientMsg)
+	pb.reader = make(chan *ClientMsg)
+	pb.update = make(chan *Update)
+	pb.repilcate = make(chan *DBCopy)
+	pb.db = make(map[string]string)
+	pb.intervals = 0
 
-  rpcs := rpc.NewServer()
-  rpcs.Register(pb)
+	rpcs := rpc.NewServer()
+	rpcs.Register(pb)
 
-  os.Remove(pb.me)
-  l, e := net.Listen("unix", pb.me);
-  if e != nil {
-    log.Fatal("listen error: ", e);
-  }
-  pb.l = l
+	os.Remove(pb.me)
+	l, e := net.Listen("unix", pb.me)
+	if e != nil {
+		log.Fatal("listen error: ", e)
+	}
+	pb.l = l
 
-  // please do not change any of the following code,
-  // or do anything to subvert it.
+	// please do not change any of the following code,
+	// or do anything to subvert it.
 
-  go func() {
-    for pb.dead == false {
-      conn, err := pb.l.Accept()
-      if err == nil && pb.dead == false {
-        if pb.unreliable && (rand.Int63() % 1000) < 100 {
-          // discard the request.
-          conn.Close()
-        } else if pb.unreliable && (rand.Int63() % 1000) < 200 {
-          // process the request but force discard of reply.
-          c1 := conn.(*net.UnixConn)
-          f, _ := c1.File()
-          err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
-          if err != nil {
-            fmt.Printf("shutdown: %v\n", err)
-          }
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        } else {
-          pb.done.Add(1)
-          go func() {
-            rpcs.ServeConn(conn)
-            pb.done.Done()
-          }()
-        }
-      } else if err == nil {
-        conn.Close()
-      }
-      if err != nil && pb.dead == false {
-        fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
-        pb.kill()
-      }
-    }
-    DPrintf("%s: wait until all request are done\n", pb.me)
-    pb.done.Wait() 
-    // If you have an additional thread in your solution, you could
-    // have it read to the finish channel to hear when to terminate.
-    close(pb.finish)
-  }()
+	go func() {
+		for pb.dead == false {
+			conn, err := pb.l.Accept()
+			if err == nil && pb.dead == false {
+				if pb.unreliable && (rand.Int63()%1000) < 100 {
+					// discard the request.
+					conn.Close()
+				} else if pb.unreliable && (rand.Int63()%1000) < 200 {
+					// process the request but force discard of reply.
+					c1 := conn.(*net.UnixConn)
+					f, _ := c1.File()
+					err := syscall.Shutdown(int(f.Fd()), syscall.SHUT_WR)
+					if err != nil {
+						fmt.Printf("shutdown: %v\n", err)
+					}
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				} else {
+					pb.done.Add(1)
+					go func() {
+						rpcs.ServeConn(conn)
+						pb.done.Done()
+					}()
+				}
+			} else if err == nil {
+				conn.Close()
+			}
+			if err != nil && pb.dead == false {
+				fmt.Printf("PBServer(%v) accept: %v\n", me, err.Error())
+				pb.kill()
+			}
+		}
+		DPrintf("%s: wait until all request are done\n", pb.me)
+		pb.done.Wait()
+		// If you have an additional thread in your solution, you could
+		// have it read to the finish channel to hear when to terminate.
+		close(pb.finish)
+	}()
 
-  pb.done.Add(1)
-  go func() {
-    for pb.dead == false {
-      pb.tick()
-      time.Sleep(viewservice.PingInterval)
-    }
-    pb.done.Done()
-  }()
+	pb.done.Add(1)
+	go func() {
+		for pb.dead == false {
+			pb.tick()
+			time.Sleep(viewservice.PingInterval)
+		}
+		pb.done.Done()
+	}()
 
-  return pb
+	return pb
 }
